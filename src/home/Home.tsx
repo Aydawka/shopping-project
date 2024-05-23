40b6f9f16dd5169256f124fe8a9e5d8c96e4e692
aydawka <aydan.gasimova2@gmail.com>
2024-05-23 01:19:47 -0700

    feat: add forms

--- src/home/Home.tsx
+++ src/home/Home.tsx
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import { Button } from 'antd';
 import Header from "../Header"
 
 
- function Home() {
+function Home() {
+  useEffect(() => {
+    document.title = 'Home Page';
+    const script = document.createElement('script');
+    script.src = 'https://tally.so/widgets/embed.js';
+    script.async = true;
+    document.head.appendChild(script);
+
+    return () => {
+      document.head.removeChild(script);
+    };
+  }, []);
+
   return (
     <div className="home">
-        <Header />
-      <Button type="primary" className="bg-amber-700">Primary Button</Button>
+      <Header />
+      <Button type={"primary"}
+              className="mt-16 ml-16"
+              data-tally-open="n0ENDP">
+        Click me</Button>
     </div>
   );
 }
